43d80c6cb630da3abcfbc01f9fab52612d8ab6e9
fatalcoder524 <11532648+fatalcoder524@users.noreply.github.com>
2025-06-03 16:24:24 +0530

    Fix setiing default_qdisc value

--- module/service.sh
+++ module/service.sh
@@ -116,7 +116,7 @@ apply_cellular_settings() {
 
 # IPv4 TCP optimizations
 echo 1 > /proc/sys/net/ipv4/tcp_ecn 2>/dev/null
-echo pfifo_fast > /proc/sys/net/core/default_qdisc 2>/dev/null
+echo "pfifo_fast" > /proc/sys/net/core/default_qdisc 2>/dev/null
 echo 150 > /proc/sys/net/ipv4/tcp_pacing_ca_ratio 2>/dev/null
 echo 200 > /proc/sys/net/ipv4/tcp_pacing_ss_ratio 2>/dev/null
 echo 1 > /proc/sys/net/ipv4/tcp_window_scaling 2>/dev/null
